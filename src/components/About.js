bfe00c3cd9a2afc4607940e6bbbf03379834fee4
Lynn Edward McQueen <lynn.edward.mcqueen@gmail.com>
2023-05-21 16:10:13 -0600

    Adds the about functionality

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,12 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
-}
+  return (
+    <div id="about">
+      <h1>About Me</h1>
+      <p>I love web programming</p>
+      <img src= {image} alt="I made this"></img>
+    </div>
+)}
 
 export default About;
